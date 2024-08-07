b765277bf98489c73d4c1f289d5675777afbffd2
Magnus Jason <magnus@magnusjason.com>
2024-08-07 17:49:19 +1000

    Initial commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+// version 1.7.3, although not important
+import axios from 'axios'
+
+export interface Object {
+  [k: string]: unknown;
+}
+
+axios({
+  maxContentLength: 1,
+  baseURL: '',
+  method: 'get',
+  headers: {  },
+  params: {  },
+})
+
